1ae1da555a88af7f30460df56386026bf69dc57d
freeman-com <5244934@gmail.com>
2020-12-10 05:34:15 +0300

    8

--- src/main/java/com/root/accessingdatamysql/UserRepository.java
+++ src/main/java/com/root/accessingdatamysql/UserRepository.java
@@ -1,6 +1,6 @@
 package com.root.accessingdatamysql;
 
-import com.root.model.User;
+import com.root.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
